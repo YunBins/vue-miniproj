45f08839c118fdc03fd5b911bd0b51ba8780697b
YunBins <kig0632@naver.com>
2024-08-22 09:21:15 +0900

    request test

--- src/components/FooterCompo.vue
+++ src/components/FooterCompo.vue
@@ -1,11 +1,27 @@
 <template>
     <div class="footer">
         <h3>푸터</h3>
+      <button onclick="apiTest()">api test</button>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {};
+
+axios.get("https://hesil.site/api/health-check")
+    .then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+  // 오류발생시 실행
+  console.log("error");
+}).then(function() {
+  // 항상 실행
+});
+
+
+
 </script>
 
 <style scoped></style>
