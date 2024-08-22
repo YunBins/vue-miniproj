5f1aa8d7ae420ddfe97deafa1a00c948a04e1d9c
YunBins <kig0632@naver.com>
2024-08-22 09:40:49 +0900

    request 4

--- src/components/FooterCompo.vue
+++ src/components/FooterCompo.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="footer">
         <h3>푸터</h3>
-      <button @click="apiTest()">api test</button>
+      <button v-on:click="apiTest">api test</button>
     </div>
 </template>
 
 <script>
-function apiTest() {
-  fetch("https://hesil.site/api/health-check", {
-    method: "GET"
-  }).then((response) => {if(response.ok) return response.text})
-      .then(data => {
-        console.log(data);
-        console.log("22222");
-      })
-      .catch(err=>console.log(err));
+export default {
+  methods: {
+    apiTest : function apiTest() {
+      fetch("https://hesil.site/api/health-check", {
+        method: "GET"
+      }).then((response) => {if(response.ok) return response.text})
+          .then(data => {
+            console.log(data);
+            console.log("22222");
+          })
+          .catch(err=>console.log(err));
+    }
+  }
 }
+
 </script>
 
 <style scoped></style>
